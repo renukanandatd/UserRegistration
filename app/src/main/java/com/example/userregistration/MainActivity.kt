80c3632ce1d1fa9f8873e8190483b08ebf8e6e14
renukanandatd <renukananda2000@gmail.com>
2023-10-01 16:11:48 +0530

    update user activity added

--- app/src/main/java/com/example/userregistration/MainActivity.kt
+++ app/src/main/java/com/example/userregistration/MainActivity.kt
@@ -4,10 +4,18 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.userregistration.databinding.ActivityMainBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var mainBinding: ActivityMainBinding
+
+    val database : FirebaseDatabase = FirebaseDatabase.getInstance()
+    val myReference : DatabaseReference = database.reference.child("MyUsers")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
@@ -18,5 +26,30 @@ class MainActivity : AppCompatActivity() {
             val intent =Intent(this,AddUserActivity::class.java)
             startActivity(intent)
         }
+
+        retriveDataFromDatabase()
+    }
+
+    fun retriveDataFromDatabase(){
+        myReference.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for(eachUser in snapshot.children){
+                    val user = eachUser.getValue(Users::class.java)
+
+                    if(user!=null){
+                        println("userId : ${user.userId}")
+                        println("userId : ${user.userName}")
+                        println("userId : ${user.userEmail}")
+                        println("userId : ${user.userAge}")
+                        println("*************************")
+                    }
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
 }
